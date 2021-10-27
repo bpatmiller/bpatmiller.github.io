0c4176ceaf06de327ff9fe2134f7dec7cbf2df7c
Brendan Miller <brendanmillerca@gmail.com>
2021-10-26 20:45:54 -0700

    add c++ tag

--- src/pages/Gfm.tsx
+++ src/pages/Gfm.tsx
@@ -7,7 +7,7 @@ const gfm_props: WriteupProps = {
   short:
     "Simulation of multiple interacting fluids (with varying density, chemical reactions, and fluid boundary projection) in 2-dimensions. Part of a semester long undergraduate reading and research course on fluid simulation .",
   date: "2019",
-  tags: ["Fluid Simulation", "Python"],
+  tags: ["Fluid Simulation", "Python", "C++"],
   codeLink: "https://github.com/bpatmiller/gfm2d",
 };
 
