cc83f75dbc3755115144bc7a716e474b05b01c83
bpatmiller <brendan.miller@standardbots.com>
2025-09-02 11:12:14 -0700

    remove unused project page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,6 @@ import { ContentGrid } from "../components/ContentGrid";
 
 const entries = [
   { title: "Theremin", link: "theremin", img: "./theremin.png" },
-  { title: "CVTrack", link: "cvtrack", img: "./cvtrack.png" },
   { title: "Fractalize.JS", link: "fractalize", img: "/frac.png" },
   { title: "Audio Reactive GAN", link: "argan", img: "/gan.png" },
   { title: "Chessboard", link: "chess", img: "/chess.png" },
